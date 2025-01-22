1e8fe0a3bf929b198c2e6be0e052456de3348870
frazhaidry <frazhaidry24@gmail.com>
2025-01-22 23:32:04 +0530

    Updated Base_URL

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove all console logs
+        drop_debugger: true, // Remove debugger statements
+      },
+    },
+  },
 })
